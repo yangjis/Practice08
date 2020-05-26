e32a662b586f12c1796c402c724d32113126a038
hi <hi@DESKTOP-53P5LLN>
2020-05-26 12:08:17 +0900

    오타 정리

--- src/com/javaex/ex08/FriendApp.java
+++ src/com/javaex/ex08/FriendApp.java
@@ -6,7 +6,7 @@ public class FriendApp {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		Friend[] friend = new Friend[3];;
+		Friend[] friend = new Friend[3];
 		
 		System.out.println("친구를 3명 등록해 주세요.");
 		for(int i = 0 ; i < 3; i++) {
